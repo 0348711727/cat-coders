4b0f50f72a279b161949b05160a389471689482b
qtran46 <quangdoannhat.tran@dxc.com>
2024-02-15 15:46:34 +0700

    Add route 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,4 +4,7 @@ import { HomeComponent } from './core/home/home.component';
 export const routes: Routes = [
   { pathMatch: 'full', redirectTo: '', path: '' },
   { path: '', component: HomeComponent },
+  { path: 'about', loadComponent: () => import('./core/home/home.component').then((x) => x.HomeComponent) },
+  { path: 'services', loadComponent: () => import('./core/home/home.component').then((x) => x.HomeComponent) },
+  { path: '**', loadComponent: () => import('./core/not-found-404/not-found-404.component').then((x) => x.NotFound404Component) }
 ];
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FooterComponent, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   constructor(private _router: Router,) {
   }
+  isActive: boolean = false;
+  concac: string = 'concac';
   RESOURCE_SERVER_URL: string = '';
-  goToHome() {
-    this._router.navigate(['../']);
+  navigateTo(path: string) {
+    this._router.navigate([path]);
   }
+
 }
